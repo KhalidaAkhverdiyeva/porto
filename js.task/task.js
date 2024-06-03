cb57701ead6ea901945ef01d99f9beb12076fc16
Khalida <xalide.haqverdiyeva@gmail.com>
2024-06-03 19:40:13 +0400

    first commmit

---
+++ js.task/task.js
@@ -0,0 +1,80 @@
+//HOMEWORK
+
+///let a=100
+///EGer eded 33 e boulunende
+// alinan qalid 30dan
+//boyukdurse hemin qaligin 14
+//de bolunmesonden alnan qaliq
+// 10dab boyudurse hemoin qalogin 5 blunmesiinden alinan qaligi goster
+
+
+// let number  = prompt();
+
+// function divider (number){
+//     let remainder = number % 33
+
+//     if(remainder > 30){
+
+//         remainder = remainder % 14;
+
+//         if(remainder > 10)
+
+//             console.log(remainder / 5)
+
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+//TASK 2
+//User eni ve uzunlugu daxil edecek
+//eger en ve uzunluq 10dan boyukdurse duzbucaqlinin sahesini goster
+//yox eger 10dan azdirsa duzbucagin perimetrini goster
+
+
+// let width = prompt("Enter the width of the rectangular");
+// let height = prompt("Enter the height of the rectangular");
+// let squareArea = width * height;
+// let squarePerimeter = 2 * (Number(width)+ Number(height));
+
+
+// if (width > 10 && height > 10 ){
+
+//     console.log(squareArea);
+
+// } else {
+//     console.log(squarePerimeter);
+    
+// };
+
+
+
+
+
+//Task4
+//Kvardratin Saheson yazin
+//EGER kvardtarin sahesin 25 den boyukdurse hemin ededin 12 ye bolunmesinden alinan qaligi goster
+
+
+
+
+// let side = prompt()
+
+// let squareArea = side *  side 
+
+
+// if(squareArea > 25){
+
+//     console.log(squareArea % 12)
+
+// }
